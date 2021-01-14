c900b66fce2dea249302467271bd4764ae4cd580
Angus Kellett <anguskellett@hotmail.com>
2021-01-14 19:25:05 +0000

    imports and creates HEROES

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,10 +13,18 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
+  heroes = HEROES;
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+/*
+export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+}
+
+*/
